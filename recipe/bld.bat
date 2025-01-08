0facf82026812ac65b788015f07f39adf80809bc
looooo <sppedflyer@gmail.com>
2025-01-08 13:19:37 +0100

    use ninja

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,16 +3,14 @@
 set "CC=clang-cl"
 set "CXX=clang-cl"
 
-cmake -B build -S %SRC_DIR% ^
+cmake -B build -S %SRC_DIR% -G "Ninja" ^
       -D CMAKE_BUILD_TYPE="Release" ^
       -D CMAKE_INSTALL_PREFIX:FILEPATH=%LIBRARY_PREFIX%
 
 if %ERRORLEVEL% neq 0 exit 1
 
-cmake --build build -j %NUMBER_OF_PROCESSORS%
-if %ERRORLEVEL% neq 0 exit 1
+ninja -C build install
 
-cmake --install build
 if %ERRORLEVEL% neq 0 exit 1
 
 mkdir %LIBRARY_PREFIX%\bin
