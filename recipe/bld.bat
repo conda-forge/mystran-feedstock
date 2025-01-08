6643dc0ea6be775b71880d24007c5d899b918187
looooo <sppedflyer@gmail.com>
2025-01-08 18:33:12 +0100

    revert windows to mdw64_fortran

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,16 +1,8 @@
-@echo on
-
-set "CC=clang-cl"
-set "CXX=clang-cl"
-
-cmake -B build -S %SRC_DIR% -G "Ninja" ^
+cmake -G "MinGW Makefiles" -B build -S %SRC_DIR% ^
       -D CMAKE_BUILD_TYPE="Release" ^
       -D CMAKE_INSTALL_PREFIX:FILEPATH=%LIBRARY_PREFIX%
 
-if %ERRORLEVEL% neq 0 exit 1
-
-ninja -C build install
-if %ERRORLEVEL% neq 0 exit 1
+mingw32-make -C build install
 
 mkdir %LIBRARY_PREFIX%\bin
 copy %SRC_DIR%\Binaries\mystran.exe %LIBRARY_PREFIX%\bin\mystran.exe
