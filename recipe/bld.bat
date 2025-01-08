68cd4e958439c2379a40218a569c094693eed9c1
looooo <sppedflyer@gmail.com>
2025-01-08 16:46:01 +0100

    remove ninja again

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,14 +3,16 @@
 set "CC=clang-cl"
 set "CXX=clang-cl"
 
-cmake -B build -S %SRC_DIR% -G "Ninja" ^
+cmake -B build -S %SRC_DIR% -G "NMake Makefiles" ^
       -D CMAKE_BUILD_TYPE="Release" ^
       -D CMAKE_INSTALL_PREFIX:FILEPATH=%LIBRARY_PREFIX%
 
 if %ERRORLEVEL% neq 0 exit 1
 
-ninja -C build install
+cmake --build build -j %NUMBER_OF_PROCESSORS%
+if %ERRORLEVEL% neq 0 exit 1
 
+cmake --install build
 if %ERRORLEVEL% neq 0 exit 1
 
 mkdir %LIBRARY_PREFIX%\bin
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,9 @@
-cmake -B build -S ${SRC_DIR} -G "Ninja" \
+cmake -B build -S ${SRC_DIR} \
       -D CMAKE_BUILD_TYPE="Release" \
       -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX
 
-ninja -C build install
+cmake --build build -j $(nproc)
+cmake --install build
 
 mkdir -p $PREFIX/bin
 cp $SRC_DIR/Binaries/mystran $PREFIX/bin/mystran
