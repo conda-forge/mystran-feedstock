c882d4f09844cf556d1c2d4125eea0bfa41c44bf
Daniel Nachun <daniel.nachun@gmail.com>
2025-02-01 20:43:49 -0800

    fix UCRT migration

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,7 @@ cmake -G "MinGW Makefiles" -B build -S %SRC_DIR% ^
       -D CMAKE_BUILD_TYPE="Release" ^
       -D CMAKE_INSTALL_PREFIX:FILEPATH=%LIBRARY_PREFIX%
 
-mingw32-make -C build install
+make -C build install
 
 mkdir %LIBRARY_PREFIX%\bin
 copy %SRC_DIR%\Binaries\mystran.exe %LIBRARY_PREFIX%\bin\mystran.exe
